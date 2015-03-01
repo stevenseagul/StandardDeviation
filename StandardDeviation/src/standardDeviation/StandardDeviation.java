0771659329c140d151e363626c7bd8a995456001
stevenseagul <jared.h.j@hotmail.com>
2015-03-01 15:37:31 -0800

    first commit, nothing here

---
+++ StandardDeviation/src/standardDeviation/StandardDeviation.java
@@ -0,0 +1,18 @@
+package standardDeviation;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class StandardDeviation 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public StandardDeviation()
+	{
+		
+	}
+}
